85528e7e0bb8ee547872413a90509096539c9b86
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2024-02-12 17:13:19 +0100

    Subido ejemplo HTTP

--- ut04/ServidorHTTP.java
+++ ut04/ServidorHTTP.java
@@ -1,4 +1,14 @@
-package ut04servicios;
+package ut04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketImpl;
+import java.nio.Buffer;
 
 /***
  * 
@@ -7,13 +17,49 @@ package ut04servicios;
  */
 public class ServidorHTTP {
 
-	/***
-	 * 
-	 * @param args
-	 * 	[0] Config file
-	 */
-	public static void main(String[] args) {
-		
+	private static final int DEFAULT_PORT = 8765;
+	private static final int RESOURCE_POSITION = 1;
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(DEFAULT_PORT);
+
+		while (true){
+			Socket connCliente = server.accept();
+			BufferedReader reader = new BufferedReader(
+										new InputStreamReader(
+											connCliente.getInputStream()
+										)
+									);
+			String header = reader.readLine();
+			System.out.println(header);
+			// GET ________ HTTP/1.1
+			String info = extraeInformacion(header);
+			String html = generaPagina(info);
+			
+			BufferedWriter writer = new BufferedWriter(
+										new OutputStreamWriter(
+											connCliente.getOutputStream()
+										)
+									);
+			// Escribir cabecera
+			writer.write("HTTP/1.1 200 OK\n");
+			//writer.write("Content-Type: application/json; charset=utf-8\n");
+			writer.write("\n");
+			writer.write(html);
+			writer.flush();
+
+			reader.close();
+			writer.close();
+			connCliente.close();
+		}
 	}
 
+	private static String generaPagina(String info) {
+		return String.format("<h1>Has pedido <b>%s</b></h1>", info);
+		//return String.format("{\"cosa\": \"%s\"}", info);
+	}
+
+	private static String extraeInformacion(String header) {
+		return header.split(" ")[RESOURCE_POSITION];
+	}
 }
